b2e9eb30bd4b3844f7fcb5bb0c13dc0a3c9135d2
SalityR <5550305sality@gmail.com>
2024-06-16 11:45:59 +0300

    save

---
+++ goit-cs-hw-01/dz2/main.py
@@ -0,0 +1,27 @@
+from lexer import Lexer
+from lexer import TokenType
+from lexer import SyntaxError
+from lexer import LexicalError
+
+from parser import Parser
+from interpreter import Interpreter
+
+
+def main():
+    while True:
+        try:
+            text = input('Введіть вираз (або "exit" для виходу): ')
+            if text.lower() == "exit":
+                print("Вихід із програми.")
+                break
+            lexer = Lexer(text)
+            parser = Parser(lexer)
+            interpreter = Interpreter(parser)
+            result = interpreter.interpret()
+            print(result)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
